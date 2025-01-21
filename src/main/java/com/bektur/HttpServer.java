c4b2979603864fb12d764f506cf426c089db1253
Bektur13 <duyshembekov04@mail.ru>
2025-01-20 16:28:35 -0800

    Uploaded configurations

---
+++ src/main/java/com/bektur/HttpServer.java
@@ -0,0 +1,8 @@
+package com.bektur;
+
+// Driver class for the HTTP Server
+public class HttpServer {
+    public static void main(String[] args) {
+        System.out.println("Starting");
+    }
+}
